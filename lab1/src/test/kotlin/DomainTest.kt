4c6fa4e00964993f5f1bb2940ab0f978adc853c1
YDiamond1 <dima.vyatkin.00@mail.ru>
2021-03-02 23:11:43 +0300

    lab1 added more tests

--- lab1/src/test/kotlin/DomainTest.kt
+++ lab1/src/test/kotlin/DomainTest.kt
@@ -11,6 +11,31 @@ class DomainTest {
 
         assertTrue(dolphin > human)
     }
+
+    @Test
+    fun `Dolphin dont have enough achievements`(){
+        val dolphin = Dolphin(arrayOf(HangOut()), Planet("Earth"))
+        val human = Human("Pavel", arrayOf(City("NewYork"), War("WWW"), Wheel()), Planet("Earth"))
+
+        assertFalse(dolphin > human)
+    }
+
+    @Test
+    fun `Dolphin dont have enough achievements 2`(){
+        val dolphin = Dolphin(arrayOf(Splashing()), Planet("Earth"))
+        val human = Human("Pavel", arrayOf(City("NewYork"), War("WWW"), Wheel()), Planet("Earth"))
+
+        assertFalse(dolphin > human)
+    }
+
+    @Test
+    fun `Dolphin is not on earth`(){
+        val dolphin = Dolphin(arrayOf(HangOut(), Splashing()), Planet("Venera"))
+        val human = Human("Pavel", arrayOf(City("NewYork"), War("WWW"), Wheel()), Planet("Earth"))
+
+        assertFalse(dolphin > human)
+    }
+
     @Test
     fun `Human more clever than dolphins`(){
         val dolphin = Dolphin(arrayOf(HangOut(), Splashing()), Planet("Earth"))
@@ -18,4 +43,37 @@ class DomainTest {
 
         assertTrue(human > dolphin)
     }
+
+    @Test
+    fun `Human dont have enough achievements`(){
+        val dolphin = Dolphin(arrayOf(HangOut(), Splashing()), Planet("Earth"))
+        val human = Human("Pavel", arrayOf(City("NewYork"),  Wheel()), Planet("Earth"))
+
+        assertFalse(human > dolphin)
+    }
+
+    @Test
+    fun `Human dont have enough achievements 2`(){
+        val dolphin = Dolphin(arrayOf(HangOut(), Splashing()), Planet("Earth"))
+        val human = Human("Pavel", arrayOf( War("WWW"), Wheel()), Planet("Earth"))
+
+        assertFalse(human > dolphin)
+    }
+
+    @Test
+    fun `Human dont have enough achievements 3`(){
+        val dolphin = Dolphin(arrayOf(HangOut(), Splashing()), Planet("Earth"))
+        val human = Human("Pavel", arrayOf(City("NewYork"), War("WWW")), Planet("Earth"))
+
+        assertFalse(human > dolphin)
+    }
+
+    @Test
+    fun `Human is not on earth` (){
+        val dolphin = Dolphin(arrayOf(HangOut(), Splashing()), Planet("Earth"))
+        val human = Human("Pavel", arrayOf(City("NewYork"), War("WWW")), Planet("Mars"))
+
+        assertFalse(human > dolphin)
+    }
+
 }
